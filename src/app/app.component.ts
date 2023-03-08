c4afe5c99b363ee49c41ec6830de73813e2f5c46
Sebastian <stonsrider@gmail.com>
2023-03-08 08:54:14 -0500

    Validate if the user is logg in with Firebase o no

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { HttpClient } from '@angular/common/http';
 import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 
@@ -9,4 +11,28 @@ import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent {
   title = 'control-clientes';
+
+  userLogged = false;
+  userApi = false;
+
+  constructor(private afAuth: AngularFireAuth,
+              private http: HttpClient) { }
+
+  ngOnInit() {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.userLogged = true;
+      } else {
+        this.userLogged = false;
+      }
+    })
+
+    this.http.get('https://ultraenvios.azurewebsites.net/api/Customer').subscribe((data) => {
+      if(data){
+        this.userApi = true;
+      }else{
+        this.userApi = false;
+      }
+    })
+  }
 }
